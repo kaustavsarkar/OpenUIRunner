ac556568d878afdf54a3e38e9c32bfd1fd915725
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2021-01-25 02:39:56 +0530

    Formatting the file

--- core/src/main/java/org/our/configuration/OurConfiguration.java
+++ core/src/main/java/org/our/configuration/OurConfiguration.java
@@ -27,6 +27,7 @@ public abstract class OurConfiguration {
 
     /**
      * Returns a custom implementation of {@link WebDriverProvider}.
+     *
      * @return {@link WebDriverProvider}.
      */
     public abstract WebDriverProvider getWebDriverProvider();
@@ -34,6 +35,7 @@ public abstract class OurConfiguration {
     /**
      * Returns an object of {@link WebDriver} depending upon the driver being
      * used by the user.
+     *
      * @return {@link WebDriver}.
      */
     public abstract WebDriver getWebDriver();
@@ -41,12 +43,14 @@ public abstract class OurConfiguration {
     /**
      * Returns the launch URL to be tested by the user. This is the URL which
      * shall be launched first while testing an application.
+     *
      * @return launchUrl - String.
      */
     public abstract String getLaunchUrl();
 
     /**
      * Returns the path where data for testing environment has been saved.
+     *
      * @return relative path of csv files.
      */
     public abstract String getRelDataPath();
@@ -55,12 +59,14 @@ public abstract class OurConfiguration {
      * Returns a regular expression for the path from where story files shall be
      * read from. If a path is not provided, all the story files are read and
      * executed.
+     *
      * @return {@link Optional<String>}
      */
     public abstract Optional<String> getStoryRegex();
 
     /**
      * Returns a list of tags added to the story file.
+     *
      * @return {@link Optional<List<String>>}.
      */
     public abstract Optional<List<String>> getIncludeTags();
@@ -68,18 +74,21 @@ public abstract class OurConfiguration {
     /**
      * Returns an enum {@link DriverName} as provided by the client while
      * running OUR.
+     *
      * @return {@link DriverName}.
      */
     public abstract DriverName getDriverName();
 
     /**
      * Returns the path where the driver is present on the file system.
+     *
      * @return - path of web driver.
      */
     public abstract String getWebDriverPath();
 
     /**
      * Returns the path where the reports shall be saved.
+     *
      * @return path to save reports.
      */
     public abstract String getReportPath();
@@ -90,25 +99,28 @@ public abstract class OurConfiguration {
          * To be used internally in the builder for setting a custom
          * implementation of {@link WebDriverProvider} created based on the
          * value provided by the user via {@link DriverName}.
-         * @param webDriverProvider - custom implementation of
-         * {@link WebDriverProvider}.
+         *
+         * @param webDriverProvider - custom implementation of {@link
+         *                          WebDriverProvider}.
          */
         abstract Builder setWebDriverProvider(
                 WebDriverProvider webDriverProvider);
 
         /**
-         * To be used internally to set the {@link WebDriver} client intends
-         * to use for testing the application. It shall be determined by the
-         * input value of {@link DriverName}.
+         * To be used internally to set the {@link WebDriver} client intends to
+         * use for testing the application. It shall be determined by the input
+         * value of {@link DriverName}.
+         *
          * @param webDriver - driver based on the browser the client shall be
-         *                 testing their application on.
+         *                  testing their application on.
          */
         abstract Builder setWebDriver(WebDriver webDriver);
 
         /**
-         * Sets the Url which shall be launched when the web driver starts.
-         * This should be the entry point from where the testing is required
-         * to be done.
+         * Sets the Url which shall be launched when the web driver starts. This
+         * should be the entry point from where the testing is required to be
+         * done.
+         *
          * @param launchUrl - entry point of the application from where the
          *                  testing needs to start.
          */
@@ -119,12 +131,14 @@ public abstract class OurConfiguration {
          * stored inside the resources/data/ folder. The method assumes this
          * shall be the base path for the data files. Client may add further
          * nested folders for storing data for different test cases.
+         *
          * @param relDataPath - data path relative to /resources/data.
          */
         public abstract Builder setRelDataPath(String relDataPath);
 
         /**
          * // TODO(kaustav): Add some doc here.
+         *
          * @param storyRegex
          * @return
          */
@@ -136,10 +150,9 @@ public abstract class OurConfiguration {
         public abstract Builder setWebDriverPath(String webDriverPath);
 
         /**
-         * Sets the path in the file system where the reports are required to
-         * be saved.
-         * Note: This method is not platform agnostic, hence, the format to
-         * provide the FS path shall be different for, say, Windows, Mac and
+         * Sets the path in the file system where the reports are required to be
+         * saved. Note: This method is not platform agnostic, hence, the format
+         * to provide the FS path shall be different for, say, Windows, Mac and
          * linux.
          */
         public abstract Builder setReportPath(String reportPath);
