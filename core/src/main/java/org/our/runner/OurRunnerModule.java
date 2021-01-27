429185247a75aca6201e20df935ced890400cca1
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2021-01-28 03:08:52 +0530

    Creating a module to be injected using a child module. The parent injector passed in the constructor shall be used by the StepFactory to create instances

--- core/src/main/java/org/our/runner/OurRunnerModule.java
+++ core/src/main/java/org/our/runner/OurRunnerModule.java
@@ -1,2 +1,24 @@
-package org.our.runner;public class OurRunnerModule {
+package org.our.runner;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
+import com.google.inject.Provides;
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.steps.Steps;
+import org.our.runner.stepfactory.CustomStepFactory;
+
+public class OurRunnerModule extends AbstractModule {
+    private final Injector injector;
+
+    public OurRunnerModule(Injector injector) {
+        this.injector = injector;
+    }
+
+    @Provides
+    CustomStepFactory provideStepsFactory(Configuration configuration,
+                                          Class<? extends Steps> root) {
+        return new CustomStepFactory(configuration, root,
+                injector);
+    }
+
 }
