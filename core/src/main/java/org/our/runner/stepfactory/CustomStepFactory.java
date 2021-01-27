95d751f5bf985beaaf773c14f7c314ccfd039d54
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2021-01-28 03:07:34 +0530

    Creating instances of actions using guice injector to enable injection in the action files.

--- core/src/main/java/org/our/runner/stepfactory/CustomStepFactory.java
+++ core/src/main/java/org/our/runner/stepfactory/CustomStepFactory.java
@@ -1,59 +1,29 @@
 package org.our.runner.stepfactory;
 
-import org.our.actions.WebDriverScreenShotWorker;
-import org.our.actions.OurAction;
-import org.our.configuration.OurConfiguration;
+import com.google.inject.Injector;
 import org.jbehave.core.configuration.Configuration;
-import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.ScanningStepsFactory;
-import org.openqa.selenium.WebDriver;
+import org.jbehave.core.steps.Steps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.our.selenium.webdriver.WebDriverProvider;
-
-import java.lang.reflect.Constructor;
 
 public class CustomStepFactory extends ScanningStepsFactory {
     private static final Logger logger =
             LoggerFactory.getLogger(CustomStepFactory.class);
-    private final OurConfiguration ourConfiguration;
-    private final StoryReporterBuilder storyReporterBuilder;
+    private final Injector injector;
 
-    public CustomStepFactory(Configuration configuration, Class<?> root,
-                             OurConfiguration ourConfiguration) {
-        this(configuration, root, ourConfiguration, new StoryReporterBuilder());
-    }
-
-    public CustomStepFactory(Configuration configuration, Class<?> root,
-                             OurConfiguration ourConfiguration,
-                             StoryReporterBuilder storyReporterBuilder) {
+    public CustomStepFactory(Configuration configuration,
+                             Class<? extends Steps> root,
+                             Injector injector) {
         super(configuration, root);
-        this.ourConfiguration = ourConfiguration;
-        this.storyReporterBuilder = storyReporterBuilder;
+        this.injector = injector;
     }
 
     @Override
     public Object createInstanceOfType(Class<?> type) {
         Object instance;
-        Constructor<?> constructor;
         try {
-            if (type.equals(WebDriverScreenShotWorker.class)) {
-                constructor =
-                        type.getDeclaredConstructor(WebDriverProvider.class,
-                                StoryReporterBuilder.class);
-                instance = constructor
-                        .newInstance(this.ourConfiguration.getDriverProvider(),
-                                storyReporterBuilder);
-            } else if (type.equals(OurAction.class)) {
-                constructor =
-                        type.getDeclaredConstructor(OurConfiguration.class);
-                constructor.setAccessible(true);
-                instance = constructor.newInstance(ourConfiguration);
-            } else {
-                constructor = type.getDeclaredConstructor(WebDriver.class);
-                instance = constructor
-                        .newInstance(this.ourConfiguration.getDriver());
-            }
+            instance = injector.getInstance(type);
         } catch (Exception e) {
             logger.error("Failed while creating instance of Actions", e);
             throw new StepsInstanceNotFound(type, this);
