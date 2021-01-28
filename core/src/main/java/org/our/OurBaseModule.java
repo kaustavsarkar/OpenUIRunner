257e1b19d66f8e0daa598156fef16fb4e732a619
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2021-01-29 01:26:04 +0530

    Refactoring OurBaseModule to break down the single method into multiple providers

--- core/src/main/java/org/our/OurBaseModule.java
+++ core/src/main/java/org/our/OurBaseModule.java
@@ -6,22 +6,22 @@ import com.google.inject.Singleton;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.io.LoadFromClasspath;
-import org.jbehave.core.reporters.CrossReference;
-import org.jbehave.core.reporters.Format;
-import org.jbehave.core.reporters.StepFailureDecorator;
-import org.jbehave.core.reporters.SurefireReporter;
-import org.jbehave.core.steps.Step;
+import org.jbehave.core.io.StoryLoader;
+import org.jbehave.core.reporters.*;
 import org.jbehave.core.steps.Steps;
 import org.our.actions.BaseAction;
 import org.our.configuration.OurConfiguration;
 import org.our.configuration.OurProperties;
 import org.our.runner.OurStoryReportBuilder;
 import org.our.runner.OurStoryReporter;
-import org.our.runner.stepfactory.CustomStepFactory;
 
 import java.util.ArrayList;
 
 public class OurBaseModule extends AbstractModule {
+
+    private static final Format[] formats =
+            new Format[]{Format.HTML, Format.JSON, Format.XML, Format.TXT,
+                    Format.STATS};
 
     @Provides
     @Singleton
@@ -39,34 +39,58 @@ public class OurBaseModule extends AbstractModule {
     }
 
     @Provides
-    @Singleton
-    Configuration provideJbehaveConfiguration(OurProperties ourProperties) {
-        SurefireReporter surefireReporter = new SurefireReporter(
+    SurefireReporter provideSurefireReporter() {
+        return new SurefireReporter(
                 this.getClass(),
                 new SurefireReporter.Options("our-surefire",
                         SurefireReporter.Options.DEFAULT_NAMING_STRATEGY, true,
                         true));
+    }
 
-        CrossReference crossReference = new CrossReference();
-        Format[] formats =
-                new Format[]{Format.HTML, Format.JSON, Format.XML, Format.TXT,
-                        Format.STATS};
+    @Provides
+    StoryLoader provideStoryLoader(Class<? extends Steps> rootClass) {
+        return new LoadFromClasspath(rootClass);
+    }
+
+    @Provides
+    StepFailureDecorator provideStepFailureDecorator() {
+        return new StepFailureDecorator(
+                new OurStoryReporter());
+    }
+
+    @Provides
+    @Singleton
+    Configuration provideJbehaveConfiguration(
+            StoryLoader storyLoader,
+            OurStoryReportBuilder reportBuilder) {
+
         return new MostUsefulConfiguration()
-                .useStoryLoader(new LoadFromClasspath(BaseAction.class))
-                .useStoryReporterBuilder(
-                        new OurStoryReportBuilder()
-                                .withOutReportDirectory(
-                                        ourProperties.getReportPath())
-                                .withFormats(formats)
-                                .withReporters(new StepFailureDecorator(
-                                        new OurStoryReporter()))
-                                .withFailureTrace(true)
-                                .withSurefireReporter(surefireReporter)
-                                .withCrossReference(crossReference));
+                .useStoryLoader(storyLoader)
+                .useStoryReporterBuilder(reportBuilder);
     }
 
     @Provides
     Class<? extends Steps> provideRootAction() {
         return BaseAction.class;
+    }
+
+    @Provides
+    CrossReference provideCrossReference() {
+        return new CrossReference();
+    }
+
+    @Provides
+    StoryReporterBuilder provideStoryReportBuilder(OurProperties ourProperties,
+                                                   SurefireReporter surefireReporter,
+                                                   CrossReference crossReference,
+                                                   StepFailureDecorator stepFailureDecorator) {
+        return new OurStoryReportBuilder()
+                .withOutReportDirectory(
+                        ourProperties.getReportPath())
+                .withFormats(formats)
+                .withReporters(stepFailureDecorator)
+                .withFailureTrace(true)
+                .withSurefireReporter(surefireReporter)
+                .withCrossReference(crossReference);
     }
 }
