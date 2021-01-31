e5f10e68f48f49deb21a4cd32b481c1a6516f731
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2021-02-01 02:09:26 +0530

    Adding doc and injections

--- core/src/main/java/org/our/actions/BaseAction.java
+++ core/src/main/java/org/our/actions/BaseAction.java
@@ -1,5 +1,6 @@
 package org.our.actions;
 
+import com.google.inject.Inject;
 import org.jbehave.core.annotations.BeforeScenario;
 import org.jbehave.core.annotations.BeforeStory;
 import org.jbehave.core.steps.Steps;
@@ -7,12 +8,20 @@ import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Responsible for getting the browser to initial set up before running tests
+ * being added by the client. The actions are being performed inside {@code
+ * BaseAction#beforeStory} handle.
+ */
 public class BaseAction extends Steps {
     private static final Logger logger =
             LoggerFactory.getLogger(BaseAction.class);
 
     protected WebDriver driver;
 
+    @Inject
     public BaseAction(WebDriver driver) {
         logger.debug("inside constructor");
         logger.info("Thread name: " + Thread.currentThread().getName());
@@ -20,10 +29,20 @@ public class BaseAction extends Steps {
         this.driver = driver;
     }
 
+    /**
+     * Maximises the driver woindow and adds timeouts to be used by the
+     * {@link WebDriver} while running tests.
+     */
     @BeforeStory
     public void beforeStory() {
-        logger.debug("inside beforeStory");
+        logger.debug("inside beforeStory. Maximising window");
         this.driver.manage().window().maximize();
+        logger.info("Setting Page load timeout: " + 60);
+        logger.info("Setting page implicit wait time: " + 20);
+
+        // TODO(kaustav): Get timeouts as input from the user.
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
     @BeforeScenario
