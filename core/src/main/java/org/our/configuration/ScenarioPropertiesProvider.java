c7e5f68c468b65f205fba2d5e93479c76e2784e3
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2021-02-07 18:36:37 +0530

    Adding ScenarioPropertiesProvider to get and set scenarioProperties whenever scenario properties are required and created respectively. A singleton instance of this object is being used across the application

--- core/src/main/java/org/our/configuration/ScenarioPropertiesProvider.java
+++ core/src/main/java/org/our/configuration/ScenarioPropertiesProvider.java
@@ -1,4 +1,25 @@
 package org.our.configuration;
 
-public class ScenarioPropertiesProvider {
+import com.google.inject.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ScenarioPropertiesProvider
+        implements Provider<ScenarioProperties> {
+    private static final Logger logger =
+            LoggerFactory.getLogger(ScenarioPropertiesProvider.class);
+    private ScenarioProperties scenarioProperties;
+
+    @Override
+    public ScenarioProperties get() {
+        logger.debug(this.toString());
+        logger.debug("returning scenario property" + scenarioProperties);
+        return scenarioProperties;
+    }
+
+    public void set(ScenarioProperties scenarioProperties) {
+        logger.debug(this.toString());
+        logger.debug("Setting scenario properties: "+ scenarioProperties);
+        this.scenarioProperties = scenarioProperties;
+    }
 }
