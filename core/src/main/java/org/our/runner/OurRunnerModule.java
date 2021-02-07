96feb025557018f158afaa72dac075046f90d7b0
kaustavsarkar <kaustavsarkar.1925@gmail.com>
2021-02-07 18:47:06 +0530

    Moving providers to OurBaseModule

--- core/src/main/java/org/our/runner/OurRunnerModule.java
+++ core/src/main/java/org/our/runner/OurRunnerModule.java
@@ -7,39 +7,32 @@ import com.google.inject.Singleton;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.io.StoryLoader;
-import org.jbehave.core.reporters.*;
+import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.Steps;
-import org.openqa.selenium.WebDriver;
-import org.our.configuration.OurConfiguration;
-import org.our.configuration.OurProperties;
 import org.our.runner.stepfactory.CustomStepFactory;
-import org.our.selenium.webdriver.WebDriverProvider;
 
 /**
  * Provides objects to be used by the instance of {@link BaseStory} being
- * created inside {@link org.our.OurConfiguredRunner} and further objects which
+ * created inside {@link OurConfiguredRunner} and further objects which
  * are created and used inside {@link BaseStory}.
  */
 public class OurRunnerModule extends AbstractModule {
-    private static final Format[] formats =
-            new Format[]{Format.HTML, Format.JSON, Format.XML, Format.TXT,
-                    Format.STATS};
 
     /**
      * A child injector being provided while creating an object of {@link
-     * OurRunnerModule} inside {@link org.our.OurConfiguredRunner}.
+     * OurRunnerModule} inside {@link OurConfiguredRunner}.
      */
-    private final Injector injector;
+    private final Injector childInjector;
 
-    public OurRunnerModule(Injector injector) {
-        this.injector = injector;
+    public OurRunnerModule(Injector childInjector) {
+        this.childInjector = childInjector;
     }
 
     @Provides
     CustomStepFactory provideStepsFactory(Configuration configuration,
                                           Class<? extends Steps> root) {
         return new CustomStepFactory(configuration, root,
-                injector);
+                childInjector);
     }
 
     /**
@@ -51,9 +44,9 @@ public class OurRunnerModule extends AbstractModule {
      * be used by JBehave.
      *
      * @param storyLoader   - object is being injected from {@link
-     *                      org.our.OurBaseModule}.
+     *                      OurBaseModule}.
      * @param reportBuilder - object is being injected from {@link
-     *                      OurRunnerModule}.
+     *                      OurBaseModule}.
      */
     @Provides
     @Singleton
@@ -65,94 +58,4 @@ public class OurRunnerModule extends AbstractModule {
                 .useStoryLoader(storyLoader)
                 .useStoryReporterBuilder(reportBuilder);
     }
-
-    /**
-     * Provides an object of {@link StoryReporterBuilder} to be used while
-     * creating an object of {@link Configuration}.
-     *
-     * @param ourProperties        - object being provided from {@link
-     *                             org.our.OurBaseModule}.
-     * @param surefireReporter     - object being provided from {@link
-     *                             org.our.OurBaseModule}.
-     * @param crossReference       - object being provided by {@link
-     *                             org.our.OurBaseModule}.
-     * @param stepFailureDecorator - object being provided by {@link
-     *                             OurRunnerModule}.
-     * @return
-     */
-    @Provides
-    StoryReporterBuilder provideStoryReportBuilder(OurProperties ourProperties,
-                                                   SurefireReporter surefireReporter,
-                                                   CrossReference crossReference,
-                                                   StepFailureDecorator stepFailureDecorator) {
-        return new OurStoryReportBuilder()
-                .withOutReportDirectory(
-                        ourProperties.getReportPath())
-                .withFormats(formats)
-                .withReporters(stepFailureDecorator)
-                .withFailureTrace(true)
-                .withSurefireReporter(surefireReporter)
-                .withCrossReference(crossReference);
-    }
-
-    /**
-     * Provides an object for {@link StepFailureDecorator} to be while building
-     * an object for {@link StoryReporterBuilder}.
-     *
-     * @param ourStoryReporter - object being provided by {@link
-     *                         OurRunnerModule}.
-     */
-    @Provides
-    StepFailureDecorator provideStepFailureDecorator(
-            OurStoryReporter ourStoryReporter) {
-        return new StepFailureDecorator(ourStoryReporter);
-    }
-
-    /**
-     * Provides an object of {@link OurStoryReporter} to be used while creating
-     * an object of {@link StepFailureDecorator}.
-     * <p>
-     * The method is marked as singleton as the object shall only be required
-     * once in the application lifetime.
-     *
-     * @param ourProperties - object is being provided by {@link
-     *                      org.our.OurBaseModule}.
-     */
-    @Provides
-    @Singleton
-    OurStoryReporter provideOurStoryReporter(OurProperties ourProperties) {
-        return new OurStoryReporter(injector, ourProperties);
-    }
-
-    /**
-     * Provides an instance of {@link WebDriver} to be used in action classes.
-     * <p>
-     * The provided instance needs to be singleton so that it can be quit and
-     * closed once the testing is completed.
-     *
-     * @param ourConfiguration - object being injected by the parent injector
-     *                         inside {@link org.our.OurConfiguredRunner}.
-     */
-    @Provides
-    @Singleton
-    WebDriver provideWebDriver(OurConfiguration ourConfiguration) {
-        return ourConfiguration.getWebDriver();
-    }
-
-    /**
-     * Provides an instance of {@link WebDriver} to be used in action classes.
-     * <p>
-     * The provided instance needs to be singleton to avoid multiple instances
-     * being created.
-     *
-     * @param ourConfiguration - object being injected by the parent injector
-     *                         inside {@link org.our.OurConfiguredRunner}.
-     */
-    @Provides
-    @Singleton
-    WebDriverProvider provideWebDriverProvider(
-            OurConfiguration ourConfiguration) {
-        return ourConfiguration.getWebDriverProvider();
-    }
-
 }
